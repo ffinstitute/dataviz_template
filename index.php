4e451a1e82f9cd5f384817ff6657517279bd2e83
jambonbill <jambonbill@gmail.com>
2016-10-05 18:09:31 +0800

    UP

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<title>Dataviz title</title>
+	<title>Dataviz title by First Finance Institute</title>
+	<link rel="shortcut icon" href="img/favicon.jpg">
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/jquery-ui.css" rel="stylesheet">
+	<link href="css/d3.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
 
-<!-- Bootstrap core CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/jquery-ui.css" rel="stylesheet">
-<link href="./css/d3.css" rel="stylesheet">
-<link href="./css/main.css" rel="stylesheet">
+	<!-- Font awesome -->
+	<link href="./css/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 
-<!-- Font awesome -->
-<link href="./css/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
-
-<script src="./js/jquery.min.js"></script>
-<script src="./js/bootstrap.js"></script>
-<script src="./js/d3.v3.min.js"></script>
-<script src="./js/main.js"></script>
+	<script src="./lib/jquery.min.js"></script>
+	<script src="./lib/bootstrap.js"></script>
+	<script src="./lib/d3.v3.min.js"></script>
+	<script src="./js/main.js"></script>
 
 </head>
 
@@ -24,8 +24,8 @@
 
 <div class='container bs-docs-container'>
 
-	<h2><img src='img/logo_ffi300px.png'> Dataviz template</h2>
-
+	<h1><a href='https://first-finance.institute' target=_blank><img src='img/logo_ffi_48px.png' title='First Finance Institute' width=40></a> Dataviz template</h1>
+	<hr />
 	<?php 
 	include "main.php";
 	?>
